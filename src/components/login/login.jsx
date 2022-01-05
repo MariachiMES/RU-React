9b240ecba3a3d59a5f24f91c74912dfef5db8b8f
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-05 11:12:22 -0600

    login and sign up styled

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -4,12 +4,25 @@ export function Login() {
   return (
     <div className="login">
       <div className="wrapper">
-        <div className="left"></div>
+        <div className="left">
+          <h1>Reunification Flow</h1>
+          <h3>Case Manager Supplemental Worflow Platform</h3>
+          <h2>SIGN-UP</h2>
+        </div>
         <div className="right">
+          <div className="titles">
+            <h2>Login</h2>
+          </div>
           <form className="login">
-            <input></input>
-            <input></input>
-            <button></button>
+            Email:
+            <input placeholder="Email" type="text" className="email"></input>
+            Password:
+            <input
+              placeholder="Password"
+              type="password"
+              className="password"
+            ></input>
+            <button>Login</button>
           </form>
         </div>
       </div>
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,5 +1,39 @@
 import "./signup.scss";
 
 export function Signup() {
-  return <div className="signup"></div>;
+  return (
+    <div className="signup">
+      <div className="signin">
+        <div className="wrapper">
+          <div className="left">
+            <h1>Welcome!!</h1>
+            <h3>If you are an existing user, you may sign-in below</h3>
+            <h2>Login</h2>
+          </div>
+          <div className="right">
+            <div className="titles">
+              <h2>Sign-Up</h2>
+            </div>
+            <form className="login">
+              Name:
+              <input
+                placeholder="First and Last Name"
+                type="text"
+                className="username"
+              ></input>
+              Email:
+              <input placeholder="Email" type="text" className="email"></input>
+              Password:
+              <input
+                placeholder="Password"
+                type="password"
+                className="password"
+              ></input>
+              <button>Sign-Up</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
