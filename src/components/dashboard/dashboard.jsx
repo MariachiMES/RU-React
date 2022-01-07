0d20efd6cf4ffcb2c2f0af6cb2037f3fa3cb7584
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-06 20:22:26 -0600

    dashboard completely styled

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -13,16 +13,16 @@ export default function Dashboard() {
             <div className="case-status-header">
               <h2>Case Status</h2>
             </div>
-            <div className="tile-contents">
-              <h3>FUCK</h3>
-              <ul>
-                <li>DATE</li>
-              </ul>
+            <div className="case-status-contents">
+              <h3>STATUS</h3>
             </div>
           </div>
           <div className="exception">
             <div className="exception-header">
               <h2>Exception</h2>
+            </div>
+            <div className="exception-contents">
+              <h2>Enter notes here</h2>
             </div>
           </div>
         </div>
@@ -31,15 +31,56 @@ export default function Dashboard() {
             <div className="demographics-header">
               <h2>Demographics</h2>
             </div>
+            <div className="demographics-content">
+              <table>
+                <tr>
+                  <td>DOB:</td>
+                  <td>COO:</td>
+                </tr>
+                <tr>
+                  <td>LOS:</td>
+                  <td>Age:</td>
+                </tr>
+                <tr>
+                  <td>Cagetgory:</td>
+                  <td>Gender:</td>
+                </tr>
+                <tr>
+                  <td>SIR:</td>
+                </tr>
+              </table>
+            </div>
           </div>
           <div className="sponsor">
             <div className="sponsor-header">
               <h2>Sponsor</h2>
             </div>
+            <div className="sponsor-contents">
+              <table>
+                <tr>
+                  <td>Name:</td>
+                  <td>Relationship:</td>
+                </tr>
+                <tr>
+                  <td>Address:</td>
+                  <td>City:</td>
+                </tr>
+                <tr>
+                  <td>City:</td>
+                  <td>State:</td>
+                </tr>
+                <tr>
+                  <td>Zip:</td>
+                </tr>
+              </table>
+            </div>
           </div>
           <div className="smarty-streets">
             <div className="smarty-streets-header">
-              <h2>Address Verification</h2>
+              <h2>Utilities</h2>
+            </div>
+            <div className="smarty-streets-contents">
+              <button>Smarty Streets</button>
             </div>
           </div>
         </div>
@@ -49,26 +90,80 @@ export default function Dashboard() {
               <h2>Taskbar</h2>
             </div>
             <div className="tasks">
-              <ul>
-                <li>Sponsor Assessment</li>
-                <li>FRP</li>
-                <li>ARI</li>
-                <li>Proof of Relationship</li>
-                <li>Proof of Address</li>
-                <li>Letter of Designation</li>
-                <li>LOPC</li>
-                <li>Sponsor Background Check</li>
-                <li>Sponsor ID Received</li>
-                <li>Fingerprints Taken</li>
-                <li>HHM ID's Received</li>
-                <li>HHM Background Checks</li>
-                <li>HHM Sex-Offender Checks</li>
-                <li>Home Country Caregiver Contact</li>
-                <li>CA/N Checks Requested</li>
-                <li>Prior Sponsorship</li>
-                <li>Previous Address</li>
-                <li>Criminal History</li>
-              </ul>
+              <table>
+                <tr>
+                  <td>Spons. Assessment</td>
+                  <td>11/12/22</td>
+                </tr>
+                <tr>
+                  <td>FRP</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>ARI</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>LOD</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Genogram Complete</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Proof of Address</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Sponsor Background</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>LOPC</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Sponsor ID Received</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Fingerprints Taken</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>HHM ID's Received</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>HHM Background</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>HHM SO Check</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Caregiver Contact</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Prior Sponsorship</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Prior Address</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>Criminal History</td>
+                  <td>DATE</td>
+                </tr>
+                <tr>
+                  <td>CA/N Checks Req.</td>
+                  <td>DATE</td>
+                </tr>
+              </table>
             </div>
           </div>
         </div>
