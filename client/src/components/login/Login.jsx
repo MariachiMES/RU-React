c510a2987d7609484346bbf432fade97d0bc9a3e
unknown <amery.bur410@gmail.com>
2022-01-15 11:18:13 -0600

    fixing resposivness for sign-up page.

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
       <div className="wrapper">
         <div className="left">
           <h1>Reunify</h1>
-          <h3>Case Manager Supplemental Worflow Platform</h3>
+          <h3>Case Manager Supplemental Workflow Platform</h3>
           <a href="/signup">
             <h2>SIGN-UP</h2>
           </a>
@@ -15,7 +15,7 @@ export default function Login() {
           <div className="titles">
             <h2>Login</h2>
           </div>
-          <form className="login">
+          <form className="login-form">
             Email:
             <input placeholder="Email" type="text" className="email"></input>
             Password:
