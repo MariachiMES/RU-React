06840092cf1775ac41ce8a311e3d58efaad421d5
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-17 13:01:09 -0600

    querying data on census table working

--- client/src/components/census/Census.jsx
+++ client/src/components/census/Census.jsx
@@ -3,12 +3,23 @@ import NewUacModal from "../newUACModal/NewUacModal";
 import Navbar from "../navbar/Navbar";
 import { useState } from "react";
 import Menu from "../menu/Menu";
+import { useQuery } from "@apollo/client";
 import { withRouter } from "react-router";
+import { QUERY_CENSUS, QUERY_USER } from "../../utils/queries";
+import { useParams } from "react-router-dom";
 
 function Census() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [newUacModalOpen, setNewUacModalOpen] = useState(false);
-
+  const { _id, uacname, a_number, submitted, approved, remanded, discharged } =
+    useParams();
+  const { loading, data, error } = useQuery(QUERY_CENSUS);
+  console.log(data);
+  const minor = data?.minors || {};
+  console.log(minor[4]._id);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="census">
       <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Navbar>
@@ -61,7 +72,7 @@ function Census() {
             <th className="mobile-invisible">Remanded</th>
           </tr>
           <tr>
-            <td>233343540</td>
+            <td key={minor._id}>{minor.a_number}</td>
             <td>Enzo Enrique Ortiz De La Madre Y Media</td>
             <td className="mobile-invisible">David Ortiz</td>
             <td className="mobile-invisible">11/23/2021</td>
